5b6c1cd4c49a56625f394bf0ed9cfc225a3c9588
Johan Lyheden <Johan.Lyheden@UNIBET.com>
2014-10-30 12:45:30 +0100

    remove vagrant config and redundant test files

--- tests/server.pp
+++
@@ -1,8 +0,0 @@
-class { '::go::server':
-  ensure              => present,
-  manage_package_repo => true,
-  manage_user         => true,
-  force               => true,
-  autoregister        => true,
-  autoregister_key    => 'foobar'
-}
